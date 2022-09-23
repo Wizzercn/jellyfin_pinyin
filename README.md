adf0d91cef1e3eafa68ba7afa47c7a50ce7089f7
Wizzer <wizzer@qq.com>
2022-09-23 17:19:45 +0800

    init v1.0

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # jellyfin_pinyin
 Jellyfin 标题拼音处理 支持客户端按拼音字母排序
+# 使用说明
+启动服务时会自动运行，其后按配置的间隔时间周期性执行
+
+# docker 环境变量说明
+
+变量名称 | 默认值 | 说明
+----|------|----
+URL | http://127.0.0.1:8096  | 服务器地址(末尾不加/)
+KEY | API KEY  | Jellyfin-控制台-高级-API密钥 添加
+MEDIA | 空  | 要扫描的媒体库名称以英文 , 分割，如 电影,动画
+TIME | 3600  | 周期扫描的时间间隔，单位秒
+
+
+# 编译说明
+
+* `mvn package nutzboot:shade` 生成可执行jar包
+* `docker build -t wizzer/jellyfin_pinyin:v1.0 . --platform=linux/amd64` Docker打包发布
---
+++ src/main/java/cn/wizzer/jellyfin/pinyin/MainLauncher.java
@@ -0,0 +1,26 @@
+package cn.wizzer.jellyfin.pinyin;
+
+import org.nutz.boot.NbApp;
+import org.nutz.ioc.Ioc;
+import org.nutz.ioc.loader.annotation.Inject;
+import org.nutz.ioc.loader.annotation.IocBean;
+
+/**
+ * @author wizzer.cn
+ */
+@IocBean(create = "init")
+public class MainLauncher {
+    @Inject("refer:$ioc")
+    private Ioc ioc;
+
+    public static void main(String[] args) throws Exception {
+        NbApp nb = new NbApp().setArgs(args).setPrintProcDoc(true);
+        nb.getAppContext().setMainPackage("cn.wizzer");
+        nb.run();
+    }
+
+    public void init() throws Exception {
+        ioc.get(JellyfinHandle.class).init();
+    }
+
+}
